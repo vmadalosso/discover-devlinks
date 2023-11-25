8cb461b4c58c48cd9df4757d03f986759571fc28
Vitor Madalosso <vitorconnected@gmail.com>
2023-11-25 17:47:11 -0500

    :tada: Initial commit

---
+++ script.js
@@ -0,0 +1,21 @@
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle("light")
+
+  // selecionar a tag img
+  const img = document.querySelector("#profile img")
+
+  // substituir a imagem
+  if (html.classList.contains("light")) {
+    // se tiver light mode, adicionar a imagem light
+    img.setAttribute("src", "./assets/avatar-light.png")
+    img.setAttribute(
+      "alt",
+      "Foto de Vitor Madalosso usando barba e oculos escuro"
+    )
+  } else {
+    //se tiver sem light mode, manter a imagem normal
+    img.setAttribute("src", "./assets/avatar.png")
+    img.setAttribute("alt", "Foto de Vitor Madalosso usando barba.")
+  }
+}
